45d66cba2cb4f285d70ea56efedbec4c04d16b19
WebDeveloper <mvunam@gmail.com>
2024-01-21 07:14:20 +0200

    DateTime Format, still needs to be fixed, but working.

--- Chicken-Dust.BusLogic/CalculateDistance.cs
+++ Chicken-Dust.BusLogic/CalculateDistance.cs
@@ -49,7 +49,7 @@ namespace Chicken_Dust.BusLogic
                 {
                     // Parse the response and extract distance information
                     string responseBody = await response.Content.ReadAsStringAsync();
-                    double distance = ParseDistanceDuration(responseBody);
+                    string distance = ParseDistanceDuration(responseBody);
 
                     // Assign the distance to the field if needed
                     // this.distance = distance;
@@ -91,7 +91,7 @@ namespace Chicken_Dust.BusLogic
                 throw new Exception("Error processing JSON response", ex);
             }
         }
-        private double ParseDistanceDuration(string jsonResponse)
+        private string ParseDistanceDuration(string jsonResponse)
         {
             // Implement logic to parse JSON and extract distance information
 
@@ -104,8 +104,8 @@ namespace Chicken_Dust.BusLogic
                 // to find the appropriate field containing the distance information.
                 // This example assumes that the distance is in the 'resourceSets[0].resources[0].results[0].travelDistance' field.
                 double distance = json.resourceSets[0].resources[0].results[0].travelDuration;
-
-                return distance;
+                string formattedTravelTime = FormatDuration(distance);
+                return formattedTravelTime;
             }
             catch (Newtonsoft.Json.JsonException ex)
             {
@@ -118,5 +118,17 @@ namespace Chicken_Dust.BusLogic
                 throw new Exception("Error processing JSON response", ex);
             }
         }
+
+        private string FormatDuration(double durationInSeconds)
+        {
+            // Create a TimeSpan based on the duration in seconds
+            TimeSpan timeSpan = TimeSpan.FromSeconds(durationInSeconds);
+
+            // Format the TimeSpan as a string
+            string formattedDuration = $"{(int)timeSpan.TotalHours:D2}:{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}";
+
+            return formattedDuration;
+        }
+
     }
 }
